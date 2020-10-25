4087a7ff6150c403c4cfa658a59e32ca61775f27
pprasitanond <pprasitanond1@babson.edu>
2020-10-25 21:01:25 +0700

    initial commit

--- mbta_helper.py
+++ mbta_helper.py
@@ -3,18 +3,34 @@ MAPQUEST_BASE_URL = "http://www.mapquestapi.com/geocoding/v1/address"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
 # Your API KEYS (you need to use your own keys - very long random characters)
-MAPQUEST_API_KEY = ""
-MBTA_API_KEY = ""
+MAPQUEST_API_KEY = "6U83snPamoNXW3MWOCNJtwNnvBL6kELF"
+MBTA_API_KEY = "32f72eb4c4054f0597f2d38cf50cffc0"
 
 
 # A little bit of scaffolding if you want to use it
+url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location=Babson%20College'
+
+
+def get_url (place_name):
+    """Write a function that takes an address or place name as input and 
+    returns a properly encoded URL to make a MapQuest geocode request."""
+    pass
 
 def get_json(url):
     """
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    import urllib.request
+    import json
+    from pprint import pprint
+
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    pprint(response_data)
+
+    return response_data
 
 
 def get_lat_long(place_name):
@@ -24,7 +40,20 @@ def get_lat_long(place_name):
     See https://developer.mapquest.com/documentation/geocoding-api/address/get/
     for Mapquest Geocoding  API URL formatting requirements.
     """
-    pass
+    import urllib.request
+    import json
+    from pprint import pprint
+
+    MAPQUEST_API_KEY = "6U83snPamoNXW3MWOCNJtwNnvBL6kELF"
+    url = f'http://www.mapquestapi.com/geocoding/v1/address?key={MAPQUEST_API_KEY}&location={place_name}'
+    
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    latLng = response_data["results"][0]["locations"][0]['latLng']
+    pprint(latLng)
+
+    return latLng
 
 
 def get_nearest_station(latitude, longitude):
@@ -42,6 +71,8 @@ def find_stop_near(place_name):
     Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
     """
     pass
+
+
 
 
 def main():
