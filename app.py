3c23d1cb9c07d406bea612affba01c3f73d127ec
mbouffard1 <70182366+mbouffard1@users.noreply.github.com>
2020-10-27 19:13:08 -0400

    change files

--- app.py
+++ app.py
@@ -1,36 +1,33 @@
 from flask import Flask, render_template, request, redirect
-from mbta_helper import get_lat_long, get_nearest_station
+from mbta_helper import find_stop_near
+
+
 app = Flask(__name__)
-app.run(debug=True)
+
 
 @app.route("/")
-
-def form():
-    '''Opens page "mbta_html.html" which prompts the user for the input of the location
-    they want to find the nearest MBTA station to.
-    '''
-    return render_template("mbta_html.html")
+def index():
+    return render_template("index.html")
 
 
-@app.route("/nearest", methods=['GET', 'POST'])
+@app.route('/nearest/', methods=['GET', 'POST'])
 def nearest():
-    '''takes the input from "mbta_html.html" and uses it to find the nearest station to that location
-    then redirects to the next page for the output'''
-    place_name = request.form['location']
-    print('Location:', request.form['location'])
-    latitude = str(get_lat_long(place_name)[0])
-    longitude = str(get_lat_long(place_name)[1])
-    nearest_station = get_nearest_station(latitude, longitude)
-    print(nearest_station)
-    return redirect('/nearest_mbta')
+    if request.method == "POST":
+        place_name = str(request.form['location name'])
+        mbta_station = find_stop_near(place_name)
+        station = mbta_station[0]
+        wheelchair = mbta_station[1]
 
-
-
-@app.route("/nearest_mbta", methods = ['GET','POST'])
-def nearest_mbta():
-    '''Opens page "mbta_station.html" and tells the user the closest station and whether or not it is 
-    wheelchair accessible'''
-    return render_template("mbta_station.html")
+        if mbta_station:
+            return render_template(
+                "result.html", 
+                place_name = place_name, 
+                station = station,
+                wheelchair = wheelchair
+            )
+        else: 
+            return render_template("mbta_form.html", error = True)
     
+    return render_template("mbta_form.html", error = None)
     
 
